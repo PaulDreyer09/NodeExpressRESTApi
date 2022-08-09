a9725c8ca0013236f1a067648a5631372d2885b4
Paul <pauljacobusdreyer@gmail.com>
2022-08-09 11:08:59 +0200

    Added http get course by id

--- index.js
+++ index.js
@@ -11,16 +11,33 @@ app.put()
 app.delete()
 */
 
+const courses = [
+    {id: 1, name: 'coarse1'},
+    {id: 2, name: 'coarse1'},
+    {id: 3, name: 'coarse1'},
+];
+
 app.get('/', (req, res) => {
     res.send('Hello World!!!');
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
+    res.send(courses);
 })
 
+//Using params: returns the id provided
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id);
+    const coarse = courses.find(c => c.id === parseInt(req.params.id));
+    if(!coarse) {
+        res.status(404).send('The coarse with the given Id was not found');
+    }
+    else res.send(coarse);
+})
+
+//Using query strings
+//Example localhost:3001/api/posts/2/2?sortBy=name
+app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.query);
 })
 
 app.listen(PORT, () => {console.log(`Listening on port ${PORT}`)});
